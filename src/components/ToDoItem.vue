fef17cb2566452a64e37169de3363285981786b0
Vladislav <machnevvlad@gmail.com>
2023-08-22 20:16:22 +0400

    setup project

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Task #<button type="button" @click="count++">{{ count }}</button></h3>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="fd">
+        <h2>This's Colomn</h2>
+        <ToDoItem/>
+        <ToDoItem/>
+    </div>
+</template>
+
+<script setup>
+import ToDoItem from './ToDoItem.vue'
+</script>
+
+<style scoped>
+.fd {
+  width: 200px;
+}
+</style>
