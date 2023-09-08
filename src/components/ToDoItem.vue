cd2c4f79cb4e437816cfd0ef93d09cb8ad743d66
Vladislav <machnevvlad@gmail.com>
2023-09-08 21:19:31 +0400

    adding Prettier, EsLint, Husky, Lint-Staged

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,26 +1,26 @@
 <template>
-    <div>
-        <h3>Task 1:</h3>
-        <h3>Task 2:</h3>
-        <!-- <button type="button" @click="count++">{{ count }}</button> -->
+  <div>
+    <h3>Task 1:</h3>
+    <h3>Task 2:</h3>
+    <!-- <button type="button" @click="count++">{{ count }}</button> -->
 
-        <button type="button">New task</button>
-        <!-- <input type="text">
+    <button type="button">New task</button>
+    <!-- <input type="text">
         <input type="checkbox" name="" id=""> -->
-        
-        <br>
-        <label for="task-name">Task Name:</label>
-        <input id="task-name">
-        
-        <br>
-        <label for="status">Status:</label>
-        <select id="status">
-            <option>BackLog</option>
-            <option>In Progress</option>
-            <option>Test</option>
-            <option>Done</option>
-        </select>
-    </div>
+
+    <br />
+    <label for="task-name">Task Name:</label>
+    <input id="task-name" />
+
+    <br />
+    <label for="status">Status:</label>
+    <select id="status">
+      <option>BackLog</option>
+      <option>In Progress</option>
+      <option>Test</option>
+      <option>Done</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -28,5 +28,4 @@
 // const count = ref(0)
 </script>
 
-<style>
-</style>
+<style></style>
